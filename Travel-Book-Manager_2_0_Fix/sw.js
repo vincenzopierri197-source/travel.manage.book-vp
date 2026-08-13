542d4f4e7c8759e6be666f75a8588ac46f56b201
vincenzopierri197-source <vincenzopierri197@gmail.com>
2026-08-13 12:15:53 +0200

    Delete Travel-Book-Manager_2_0_Fix directory

--- Travel-Book-Manager_2_0_Fix/sw.js
+++
@@ -1,4 +0,0 @@
-const CACHE="tbm-v2-shell";
-self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","./index.html","./manifest.json","./travel-book-manager-logo.png","./icon-192.png","./icon-512.png"])));self.skipWaiting();});
-self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
-self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(fetch(e.request).then(r=>{const cp=r.clone();caches.open(CACHE).then(c=>c.put(e.request,cp));return r;}).catch(()=>caches.match(e.request).then(r=>r||caches.match("./index.html"))));});
